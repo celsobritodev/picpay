74736ca365c6e65a4d0811a4db4f9fcc309693cf
celsobritodev <celsinhobrito@gmail.com>
2025-09-23 09:42:54 -0300

    bug: quando envia carteira vazia 'wallet' retorna o erro, mas se for transfererencia 'transfer'  vazia não

--- src/main/java/tech/buildrun/picpay/PicpayApplication.java
+++ src/main/java/tech/buildrun/picpay/PicpayApplication.java
@@ -4,6 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+
+
+// Resolvendo DESAFIO BACKEND do PicPay com Java e Spring Boot
+// https://youtu.be/dttXo48oXt4
+
+
+
 @SpringBootApplication
 @EnableFeignClients
 public class PicpayApplication {
